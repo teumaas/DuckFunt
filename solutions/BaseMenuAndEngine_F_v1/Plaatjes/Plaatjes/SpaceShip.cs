aaffcf91a15c1b01e40097a4279dc21cc17ca908
Floris van Londen <FvLonden@gmail.com>
2015-11-25 11:10:48 +0100

    update

---
+++ solutions/BaseMenuAndEngine_F_v1/Plaatjes/Plaatjes/SpaceShip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plaatjes
+{
+    class SpaceShip
+    {
+        SpaceShip()
+        {
+
+
+        }
+        //GenSpaceShipPath()
+        //{
+           
+        //}
+        void SpawnAmountOfShips(int amount)
+        {
+        
+        }
+    }
+}
