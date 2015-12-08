607146ab72c4827aa7f6b08002eff892e9e4d9fd
Floris van Londen <FvLonden@gmail.com>
2015-12-08 12:38:52 +0100

    highscore class

---
+++ solutions/MenuMaking-WithScore_F_v1/MenuMaking/MenuMaking/CursorUpdater.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MenuMaking
+{
+
+    class CursorUpdater
+    {
+        MouseState state;
+        //public void CursorUpdater()
+        //{
+        //    state = Mouse.GetState();
+        //}
+        public int GetCursX()
+        {
+            state = Mouse.GetState();
+            return Convert.ToInt32(state.X);
+        }
+        public int GetCursY()   
+        {
+            state = Mouse.GetState();
+            int i = Convert.ToInt32(state.Y);
+            return i;
+        }
+     }   
+}
